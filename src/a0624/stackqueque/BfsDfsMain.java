cbcc025f130fb9ac0a8f3669976da2b3ff2ae6a7
MyeongIn Jin <kai5588@naver.com>
2024-06-24 10:28:31 +0900

    ArrayDeque 구현

---
+++ src/a0624/stackqueque/BfsDfsMain.java
@@ -0,0 +1,15 @@
+package a0624.stackqueque;
+
+public class BfsDfsMain {
+	static final int[] di = {}; 
+	static final int[] dj = {};
+	static int N = 5, C=0;
+	static int[][] a;
+	static boolean[][] v;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/a0624/stackqueque/StackQueueTest.java
+++ src/a0624/stackqueque/StackQueueTest.java
@@ -1,5 +1,49 @@
 package a0624.stackqueque;
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class StackQueueTest {
+	public static void main(String[] args) {
+		
+		//이제 이거 안쓰고
+		//Queue<String> queque = new LinkedList<>();
+		// 이거 씀
+		ArrayDeque<String> queque = new ArrayDeque<>();
+		queque.offer("강호동");
+		queque.offer("저팔계");
+		queque.offer("사오정");
+		queque.offer("손오공");
+		queque.offer("홍길동");
+		
+		System.out.println(queque.peek()+" "+queque.size());
+		System.out.println(queque.peek()+" "+queque.size());
+		System.out.println(queque.peek()+" "+queque.size());
+		System.out.println(queque.poll()+" "+queque.size());
+		System.out.println(queque.isEmpty());
+		System.out.println(queque.poll()+" "+queque.size());
+		System.out.println(queque);
+		
+		
+		
+		//이제 이거 안쓰고
+		//Stack<String> stack = new Stack<>();
+		ArrayDeque<String> stack = new ArrayDeque<>();
+		stack.push("강호동");
+		stack.push("저팔계");
+		stack.push("사오정");
+		stack.push("손오공");
+		stack.push("홍길동");
+		
+		System.out.println(stack.peek()+" "+stack.size());
+		System.out.println(stack.peek()+" "+stack.size());
+		System.out.println(stack.peek()+" "+stack.size());
+		System.out.println(stack.pop()+" "+stack.size());
+		System.out.println(stack.isEmpty());
+		System.out.println(stack.pop()+" "+stack.size());
+		System.out.println(stack);
+	}
 
 }
