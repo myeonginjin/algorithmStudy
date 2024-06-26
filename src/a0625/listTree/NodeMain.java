5584ad1c800e3069999ce2ec6fc69c59562193a9
MyeongIn Jin <kai5588@naver.com>
2024-06-26 10:43:25 +0900

    노드 구현

--- src/a0625/listTree/NodeMain.java
+++ src/a0625/listTree/NodeMain.java
@@ -1,6 +1,19 @@
 package a0625.listTree;
 
 public class NodeMain {
+	static class Node {
+		int vertex;
+		Node link;
+		Node(int vertex, Node link) {
+			this.vertex = vertex;
+		}
+		
+		@Override
+		public String toString() {
+			return vertex+ "->" + link;
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 	}
