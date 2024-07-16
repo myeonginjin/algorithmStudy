be2a2025b9050cc2dd02f93e7da8db8de8ea24d0
MyeongIn Jin <kai5588@naver.com>
2024-07-16 16:49:17 +0900

    팰린드롬수 판별 문제 구현

---
+++ src/study/Main_1259.java
@@ -0,0 +1,31 @@
+package study;
+
+import java.util.*;
+
+public class Main_1259 {
+	
+	static boolean check(int n) {
+		String[] t = Integer.toString(n).split("");
+		
+		
+		for (int i = 0; i<t.length/2; i++) {
+			if(!t[i].equals(t[t.length-i-1])) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int n = sc.nextInt();
+			if(n==0) break;
+			
+			String ans = check(n) ? "yes" : "no";
+			System.out.println(ans);
+		}
+	}
+
+}
