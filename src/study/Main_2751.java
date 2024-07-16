a5369b10c2f19fede8f0694a3f300f0f4398019d
MyeongIn Jin <kai5588@naver.com>
2024-07-16 17:22:10 +0900

    우선순위 큐 기본 문제

---
+++ src/study/Main_2751.java
@@ -0,0 +1,25 @@
+package study;
+
+import java.util.*;
+
+public class Main_2751 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+		for (int i = 0; i<n; i++) {
+			q.offer(sc.nextInt());
+		}
+		
+		for (int i = 0; i<n; i++) {
+			sb.append(q.poll()).append("\n");
+		}
+		
+		System.out.println(sb);
+		
+	}
+
+}
