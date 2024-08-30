7274c72ff2410725f1f609b1ebd7df51c3cf8eb9
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-08-31 03:16:38 +0900

    거듭제곱 분할정복 문제 풀

---
+++ src/study/Main_1629.java
@@ -0,0 +1,33 @@
+package study;
+
+import java.util.*;
+import java.io.*;
+
+public class Main_1629 {
+	
+	static long recur (int a, int b, int c) {
+		if(b <= 1) {
+			return a % c;
+		}
+		
+		long half = recur(a, b / 2, c);
+			
+		if(b % 2 == 1) {
+			return  (  (half * half % c) * (a%c)  ) % c;
+		}
+		
+	
+		return (half * half) % c;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		System.out.println(recur(a,b,c));
+
+		
+	}
+
+}
